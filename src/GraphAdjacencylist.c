f1d1644b17423355ffada97eea9b9986de308e07
Anshu Raghuvanshi <anshuraghuvanshi@Anshus-MacBook-Air.local>
2017-05-17 07:57:43 +0530

    Updated Graph program ShortestPathAlgorithm

--- src/GraphAdjacencylist.c
+++ src/GraphAdjacencylist.c
@@ -103,7 +103,7 @@ queueArray * createQueue()
     printf("No memory allocated");
     exit(0);
   }
-  queue->capacity=6;
+  queue->capacity=7;
   queue->front=queue->rear=-1;
   queue->arr=(int*)malloc(queue->capacity*sizeof(int));
   if(!queue->arr)
@@ -171,7 +171,7 @@ int isEmpty(queueArray *queue)
 {
     return queue->front == -1;
 }
-int topologicalSort(Graph *graph, int *arr)
+void topologicalSort(Graph *graph, int *arr)
 {
    int i,k;
    int count=0;
@@ -197,23 +197,90 @@ int topologicalSort(Graph *graph, int *arr)
        }
    }
 }
+void printarray(int *arr, int num)
+{
+   for(int i=0;i<num;i++)
+     printf("%d ",arr[i]);
+   printf("\n");
+}
+int shortestPathAlgorithm(Graph *graph, int s)
+{
+    int *distance, *path;
+    distance=(int*)malloc(sizeof(int)*graph->V);
+    path=(int*)malloc(sizeof(int)*graph->V);
+    queueArray *queue=createQueue();
+    for(int i=0;i<graph->V;i++)
+       distance[i]=-1;
+    int v,w;
+    distance[s]=0;
+    enque(queue,s);
+ 
+    while(!isEmpty(queue))
+    {
+       v=deque(queue);
+       adjacencyListNode *temp=graph->array[v].head;
+       while(temp)
+       {
+          if(distance[temp->data]==-1)
+          {
+              distance[temp->data]=distance[v]+1;
+              path[temp->data]=v;
+              enque(queue,temp->data);
+          }
+          temp=temp->next;
+       }
+    }
+    printarray(distance,graph->V);
+    printarray(path,graph->V);          
+    free(distance);
+    free(path);
+    return 0;
+}
+typedef struct storePath
+{
+    char Vertices;
+    int distance; 
+}storePath;
+Graph * addedge1(Graph *graph, char s, char d)
+{
+    adjacencyListNode *temp=createNode(d);
+    temp=graph->array[s].head;
+    graph->array[s].head=temp;
 
-
+    return graph;  
+}
 int main()
 {
     // create the graph given in above fugure
     int V = 6;
-    int arr[6]={0,0,0,0,0,0};
-    Graph* graph = createGraph(V,1);
+    int arr[7]={0,0,0,0,0,0,0};
+ /*   Graph* graph = createGraph(V,1);
     addEdge(graph,5, 2,arr);
     addEdge(graph,5, 0,arr);
     addEdge(graph,4, 0,arr);
     addEdge(graph,4, 1,arr);
     addEdge(graph,2, 3,arr);
     addEdge(graph,3, 1,arr);
+
     // print the adjacency list representation of the above graph
     printGraph(graph);
 
-    topologicalSort(graph,arr);
+    topologicalSort(graph,arr);*/
+    Graph *graph=createGraph(7,1);
+    
+    addEdge(graph,0, 1,arr);
+    addEdge(graph,0, 3,arr);
+    addEdge(graph,1, 3,arr);
+    addEdge(graph,1, 4,arr);
+    addEdge(graph,2, 0,arr);
+    addEdge(graph,2, 5,arr);
+    addEdge(graph,3, 2,arr);
+    addEdge(graph,3, 4,arr);
+    addEdge(graph,3, 5,arr);
+    addEdge(graph,3, 6,arr);
+    addEdge(graph,4, 6,arr);
+    addEdge(graph,6, 5,arr);
+    printGraph(graph);
+    shortestPathAlgorithm(graph,0);
     return 0;
 }
